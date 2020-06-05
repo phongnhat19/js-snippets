4a5df593a8d34db327c893ef4bffd602248ac680
Nhat Nguyen <phongnhat19@gmail.com>
2020-06-05 14:51:04 +0700

    clear container content

--- test-button.js
+++ test-button.js
@@ -3,5 +3,6 @@ const container = document.getElementById('button_container');
 if (container) {
   const myBtn = document.createElement('button')
   myBtn.innerHTML = 'Test Button'
+  container.innerHTML = '';
   container.appendChild(myBtn);
 }
