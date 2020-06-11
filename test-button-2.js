545ef5c4be851bec610bde59bab83bf106bd3771
Nhat Nguyen <phongnhat19@gmail.com>
2020-06-11 10:29:06 +0700

    change container variable

--- test-button-2.js
+++ test-button-2.js
@@ -1,8 +1,8 @@
-const container = document.getElementById('button_container');
+const container2 = document.getElementById('button_container');
 
-if (container) {
+if (container2) {
   const myBtn = document.createElement('button');
   myBtn.innerHTML = 'Test Button 2';
-  container.innerHTML = '';
-  container.appendChild(myBtn);
+  container2.innerHTML = '';
+  container2.appendChild(myBtn);
 }
