fea02c320713ccdcc8bdfaef18ebed9f9161d795
Nhat Nguyen <phongnhat19@gmail.com>
2020-06-11 09:50:17 +0700

    change button content

--- test-button.js
+++ test-button.js
@@ -1,8 +1,8 @@
 const container = document.getElementById('button_container');
 
 if (container) {
-  const myBtn = document.createElement('button')
-  myBtn.innerHTML = 'Test Button'
+  const myBtn = document.createElement('button');
+  myBtn.innerHTML = 'Test Button 123';
   container.innerHTML = '';
   container.appendChild(myBtn);
 }
